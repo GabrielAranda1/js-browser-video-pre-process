631069fd118a7cf01a88317f17dc7fb1a87ff1b9
GabrielAranda1 <per.gabrielaranda@gmail.com>
2023-10-27 13:11:06 -0300

    feat: decode uploaded video , then decode and print on DOM canvas

--- initial-template/app/pages/file-upload/src/app.js
+++ initial-template/app/pages/file-upload/src/app.js
@@ -4,18 +4,29 @@ import View from './view.js'
 const view = new View()
 const clock = new Clock()
 
+const worker = new Worker('./src/worker/worker.js', { type: 'module' })
+
+worker.onerror = (error) => {
+    console.error('Error in worker', error)
+}
+
+worker.onmessage = ({ data }) => {
+    if (data.status !== 'done') return
+
+    clock.stop()
+    view.updateElapsedTime(`Process took ${took.replace('ago', '')}`)
+}
+
 let took = ''
 
 view.configureOnFileChange(file => {
+    const canvas = view.getCanvas()
+    worker.postMessage({ file, canvas }, [canvas])
+
     clock.start((time) => {
         took = time;
         view.updateElapsedTime(`Process started ${time}`)
     })
-
-    setTimeout(() => {
-        clock.stop()
-        view.updateElapsedTime(`Process took ${took.replace('ago', '')}`)
-    }, 5000)
 })
 
 
